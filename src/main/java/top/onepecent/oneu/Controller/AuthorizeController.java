dcf76735f9f8f50bf59d63a5aa9f8cf5587e2115
Onepecent <1066961902@qq.com>
2021-07-07 16:34:08 +0800

    upload

--- src/main/java/top/onepecent/oneu/Controller/AuthorizeController.java
+++ src/main/java/top/onepecent/oneu/Controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package top.onepecent.oneu.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,13 +20,12 @@ import java.util.UUID;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-    //    @Value("${github.client.id}")
-    private String clientID = "dff6e34bfb612c86793b6264be6ba60a8bb3511d16cd47d52e06632a7aa0cc29";
-    //    @Value("${github.client.uri}")
-    private String clientUri = "http://101.132.149.20:8886/callback";
-//    private String clientUri = "http://101.132.149.20:8080/callback";
-    //    @Value("${github.client.secret}")
-    private String clientSecret = "3c39e87ec18424d90822cf7700a0accf67adbb51fb76b650ec50436c43999ee8";
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.uri}")
+    private String clientUri;
+    @Value("${github.client.secret}")
+    private String clientSecret;
 
     @Autowired
     private UserService userService;
@@ -41,22 +41,6 @@ public class AuthorizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        //{"id":7687367,"login":"onepecent","name":"OnePecent",
-        // "avatar_url":"https://portrait.gitee.com/uploads/avatars/user/2562/7687367_onepecent_1624536058.png",
-        // "url":"https://gitee.com/api/v5/users/onepecent",
-        // "html_url":"https://gitee.com/onepecent",
-        // "followers_url":"https://gitee.com/api/v5/users/onepecent/followers",
-        // "following_url":"https://gitee.com/api/v5/users/onepecent/following_url{/other_user}",
-        // "gists_url":"https://gitee.com/api/v5/users/onepecent/gists{/gist_id}",
-        // "starred_url":"https://gitee.com/api/v5/users/onepecent/starred{/owner}{/repo}",
-        // "subscriptions_url":"https://gitee.com/api/v5/users/onepecent/subscriptions",
-        // "organizations_url":"https://gitee.com/api/v5/users/onepecent/orgs",
-        // "repos_url":"https://gitee.com/api/v5/users/onepecent/repos",
-        // "events_url":"https://gitee.com/api/v5/users/onepecent/events{/privacy}",
-        // "received_events_url":"https://gitee.com/api/v5/users/onepecent/received_events",
-        // "type":"User","blog":null,"weibo":null,"bio":"","public_repos":0,"public_gists":0,"followers":0,
-        // "following":0,"stared":0,"watched":3,"created_at":"2020-06-13T20:34:48+08:00",
-        // "updated_at":"2021-06-24T20:00:58+08:00","email":null}
         if (githubUser != null) {
             User user = new User();
             user.setAccountId(String.valueOf(githubUser.getId()));
